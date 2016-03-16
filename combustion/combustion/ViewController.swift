f0145f5559993529f07534a8b566d2263919bfab
Lucas Andrade <passaroalado@hotmail.com>
2016-03-16 10:50:04 -0500

    collectionView

--- combustion/combustion/ViewController.swift
+++ combustion/combustion/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //rows
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 40
+    }
+    //columns
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = UICollectionViewCell()
+        
+        if indexPath.row % 2 == 0{
+            cell = collectionView.dequeueReusableCellWithReuseIdentifier("cellLeft", forIndexPath: indexPath)
+        }
+        else{
+            cell = collectionView.dequeueReusableCellWithReuseIdentifier("cellRight", forIndexPath: indexPath)
+        }
+        // Configure the cell
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets.init(top: 10, left: 10, bottom: 10, right: 10)
+    }
 }
 
