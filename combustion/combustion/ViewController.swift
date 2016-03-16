c081823795b13223333960aa8e8496bcdebbd721
Lucas Andrade <passaroalado@hotmail.com>
2016-03-16 11:42:24 -0500

    collectionview layout

--- combustion/combustion/ViewController.swift
+++ combustion/combustion/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     @IBOutlet weak var collectionView: UICollectionView!
-
+    
+    let cellSize = CGSize(width: 295, height: 203)
+    var cellScale: CGFloat!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.cellScale = (cellSize.width)/(cellSize.height)
+        
+        
+        //setting collection view
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        
+        
     
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -23,31 +34,46 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //rows
+    //rows number
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 40
     }
-    //columns
+    //columns number
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 2
     }
+    //If the column is on the right, the cell used from storyboard will be on the right, and the same for the left side
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         var cell = UICollectionViewCell()
         
+        //if the number of the column is even
         if indexPath.row % 2 == 0{
             cell = collectionView.dequeueReusableCellWithReuseIdentifier("cellLeft", forIndexPath: indexPath)
         }
+        //if the number of the column is odd
         else{
             cell = collectionView.dequeueReusableCellWithReuseIdentifier("cellRight", forIndexPath: indexPath)
         }
-        // Configure the cell
+        cell.backgroundColor = UIColor.greenColor()
+        cell.alpha = 0.6
         return cell
     }
+    //size of each column
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSize(width: 100, height: 100)
+        
+        //cell needs to be half of screen, so button will be in middle
+        let cellWidth = self.view.frame.width/2
+        //changing height dinamically with correct scale from storyboard
+        let cellHeight = cellSize.height - (cellSize.width - cellWidth)
+        return CGSize(width: cellWidth, height: cellHeight)
     }
+    //layout (distance between corners and view) of each column
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-            return UIEdgeInsets.init(top: 10, left: 10, bottom: 10, right: 10)
+            return UIEdgeInsets.init(top: 10, left: 0, bottom: 10, right: 0)
+    }
+    //space between columns
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0
     }
 }
 
