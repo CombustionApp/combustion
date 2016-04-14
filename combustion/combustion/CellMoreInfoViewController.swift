e8e806340765e282a2bc2ceb02aafa5a15f83bca
Lucas Andrade <passaroalado@hotmail.com>
2016-04-13 22:18:41 -0500

    likes and going actions

--- combustion/combustion/CellMoreInfoViewController.swift
+++ combustion/combustion/CellMoreInfoViewController.swift
@@ -15,6 +15,10 @@ class CellMoreInfoViewController: UIViewController {
     @IBOutlet weak var labelEventName: UILabel!
     @IBOutlet weak var viewHorizontal: UIView!
     @IBOutlet weak var scrollViewHorizontal: UIScrollView!
+    @IBOutlet weak var labelNumberGoing: UILabel!
+    @IBOutlet weak var imageIconIGo: UIImageView!
+    @IBOutlet weak var imageIconLike: UIImageView!
+    @IBOutlet weak var labelNumberLiking: UILabel!
     @IBOutlet weak var textViewComment: UITextView!
     @IBOutlet weak var tableViewComments: UITableView!
     @IBOutlet weak var buttonSendComment: UIButton!
@@ -39,6 +43,31 @@ class CellMoreInfoViewController: UIViewController {
     }
     
 
+    @IBAction func buttonIWillGo(sender: AnyObject) {
+        if(self.imageIconIGo.image! == UIImage(named: "iwillgo_Big")){
+            self.imageIconIGo.image =  UIImage(named: "iwillgo_Big_blue")
+            self.labelNumberGoing.text = String(Int(labelNumberGoing.text!)! + 1)
+            self.labelNumberGoing.textColor = UIColor(red: 60/255, green: 135/255, blue: 231/255, alpha: 1)
+        }
+        else{
+            self.imageIconIGo.image =  UIImage(named: "iwillgo_Big")
+            self.labelNumberGoing.text = String(Int(labelNumberGoing.text!)! - 1)
+            self.labelNumberGoing.textColor = UIColor.whiteColor()
+        }
+    }
+    @IBAction func buttonIlike(sender: AnyObject) {
+        if(self.imageIconLike.image! == UIImage(named: "like_big")){
+            self.imageIconLike.image =  UIImage(named: "like_big_red")
+            self.labelNumberLiking.text = String(Int(labelNumberLiking.text!)! + 1)
+            self.labelNumberLiking.textColor = UIColor(red: 250/255, green: 44/255, blue: 52/255, alpha: 1)
+        }
+        else{
+            self.imageIconLike.image =  UIImage(named: "like_big")
+            self.labelNumberLiking.text = String(Int(labelNumberLiking.text!)! - 1)
+            self.labelNumberLiking.textColor = UIColor.whiteColor()
+        }
+
+    }
     /*
     // MARK: - Navigation
 
